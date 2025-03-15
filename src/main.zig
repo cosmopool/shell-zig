ce6296bcb101ad48ab2de93fdb701da0a14e9205
Kaio Delphino <dash.zet@gmail.com>
2025-03-15 14:26:06 -0300

    feat: implement repl loop

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,19 @@ const std = @import("std");
 pub fn main() !void {
     const stderr = std.io.getStdErr().writer();
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
-
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    try stderr.print("{s}: command not found\n", .{user_input});
+    var shouldExit: bool = false;
+    while (!shouldExit) {
+        try stdout.print("$ ", .{});
+
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        if (std.mem.eql(u8, user_input, "exit")) {
+            shouldExit = true;
+        } else {
+            try stderr.print("{s}: command not found\n", .{user_input});
+        }
+    }
 }
