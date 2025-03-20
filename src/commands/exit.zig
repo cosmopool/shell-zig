ea26a3c61dcad77ea1c9acc4e2fa6ffea537e869
Kaio Delphino <dash.zet@gmail.com>
2025-03-20 11:06:23 -0300

    feat: accept no code from exit command

--- src/commands/exit.zig
+++ src/commands/exit.zig
@@ -3,7 +3,9 @@ const testing = @import("std").testing;
 
 pub fn run(input: [][]const u8) !void {
     std.debug.assert(std.mem.eql(u8, input[0], "exit"));
-    std.debug.assert(input.len == 2);
+    std.debug.assert(input.len > 0);
+
+    if (input.len == 1) std.process.exit(0);
 
     const code = try std.fmt.parseUnsigned(u8, input[1], 10);
     std.process.exit(code);
